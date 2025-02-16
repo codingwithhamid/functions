aed6cf21b09bb4951f856c4d89726b991e016cd1
Hamid <hamid@ahmadalisheikh267.com>
2025-02-16 22:38:46 +0500

    methods of object in  JavaScript

--- 43. Dot and Bracket in objects/script.js
+++ 43. Dot and Bracket in objects/script.js
@@ -16,11 +16,18 @@ console.log(jonas['firstName']);
 console.log(jonas['lastName']);
 
 const intrestedIn = prompt('What do you know about jonas?Choose between firstName , LastName , age , friends and job');
-console.log(jonas[intrestedIn]);
+
 
 if(jonas[intrestedIn]){
     console.log(jonas[intrestedIn]);
 }else{
-    console.log('Wrong request!Choose between firstName , LastName , age , friends and job');
+    console.log('Wrong request!Choose between firstName , LastName , age , friends and job.');
 }
 
+jonas.location = 'Pakistan';
+jonas ['email'] = 'chachabhai@gmail.com';
+console.log(jonas);
+
+
+// Challenge
+console.log(`${jonas.firstName} has ${jonas.friends.length}  friends and his best friend is ${jonas.friends[0].}`)
